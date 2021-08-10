58455ac8c8cac014827d86b94ded948964ef484a
pedrorhamon <pedrorhamon16@gmail.com>
2021-08-09 21:55:28 -0300

    Salvando Usuário no bd, part 1

--- src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -1,14 +1,21 @@
 package br.com.caelum.vraptor.controller;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.model.Usuario;
 
 @Path("dashboard")
 @Controller
 public class DashboardController {
+	
+	@Inject EntityManager em;
+	@Inject Result resul;
 	
 	@Get("")
 	public void dashboard() {
@@ -17,8 +24,7 @@ public class DashboardController {
 	
 	@Post("salvaUsuario")
 	public void salvaUsuario(Usuario usuario) {
-		System.out.println(usuario.getNome());
-		
-		
+		em.persist(usuario);
+		resul.redirectTo(DashboardController.class).dashboard();
 	}
 }
--- src/main/java/br/com/caelum/vraptor/model/Usuario.java
+++ src/main/java/br/com/caelum/vraptor/model/Usuario.java
@@ -1,7 +1,16 @@
 package br.com.caelum.vraptor.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String nome;
 	private String email;
 	private String senha;
@@ -28,5 +37,13 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 }
