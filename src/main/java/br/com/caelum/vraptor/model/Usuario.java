ac0b61ac7aa8eca1cf864a38a5c1679b1ab42f21
pedrorhamon <pedrorhamon16@gmail.com>
2021-08-10 23:10:35 -0300

    Inserindo no Projeto Model

--- src/main/java/br/com/caelum/vraptor/model/Usuario.java
+++ src/main/java/br/com/caelum/vraptor/model/Usuario.java
@@ -1,21 +1,14 @@
 package br.com.caelum.vraptor.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-public class Usuario {
+public class Usuario extends Model{
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
 	@NotEmpty 
 	@Size(min = 4, max = 20)
 	private String nome;
@@ -50,13 +43,5 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 }
